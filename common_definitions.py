53241ddacc8c3cc0a64e4abb6a65418d504a3e11
Weidows <utsuko27@qq.com>
2022-06-09 01:47:52 +0800

    🐳 test.

--- common_definitions.py
+++ common_definitions.py
@@ -1,15 +1,26 @@
+'''
+?: *********************************************************************
+Author: Weidows
+Date: 2022-02-28 22:49:55
+LastEditors: Weidows
+LastEditTime: 2022-06-09 01:39:52
+FilePath: \SinGlow\common_definitions.py
+Description:
+!: *********************************************************************
+'''
 import tensorflow as tf
 import math
 import numpy as np
 
-LOAD_WEIGHT = True
+LOAD_WEIGHT = False
 
 # model parameter
 SQUEEZE_FACTOR = 2
 K_GLOW = 12
 L_GLOW = 8
 WINDOW_SIZE = 1 # in seconds
-SAMPLING_RATE = 16384 # target sampling rate
+# SAMPLING_RATE = 16384 # target sampling rate
+SAMPLING_RATE = 200 # target sampling rate
 WINDOW_LENGTH = int(WINDOW_SIZE*SAMPLING_RATE) # better to be mult of SQUEEZE_FACTOR
 CHANNEL_SIZE = 1
 BUFFER_SIZE = 20000
@@ -21,8 +32,10 @@ KERNEL_INITIALIZER = tf.keras.initializers.he_normal()
 LEARNING_RATE = 1e-3
 LAMBDA_LIPSCHITZ = 1e-3
 DROPOUT_N = 0.1
-BATCH_SIZE = 32
-EPOCHS = 50
+# BATCH_SIZE = 32
+BATCH_SIZE = 8
+# EPOCHS = 50
+EPOCHS = 5
 CHECKPOINT_PATH = "./checkpoints/weights"
 TENSORBOARD_LOGDIR = "./logs/GLOW"
 
